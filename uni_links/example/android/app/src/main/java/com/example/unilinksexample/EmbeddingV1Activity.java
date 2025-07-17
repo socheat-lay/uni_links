0372d0700e2ba2edca51ff64a38f63c984b12f13
Socheat LAY <lay.socheat@ababank.com>
2025-07-17 10:43:19 +0700

    update gradle plugin to support new version of flutter

--- uni_links/example/android/app/src/main/java/com/example/unilinksexample/EmbeddingV1Activity.java
+++ uni_links/example/android/app/src/main/java/com/example/unilinksexample/EmbeddingV1Activity.java
@@ -4,13 +4,9 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import name.avioli.unilinks.UniLinksPlugin;
 
 public class EmbeddingV1Activity extends FlutterActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        UniLinksPlugin.registerWith(registrarFor("name.avioli.unilinks.UniLinksPlugin"));
-    }
+    
 }
